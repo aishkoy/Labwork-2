235f811170e4c0e3da78b78e9e259bc4db317548
aishkoy <aisha.orozbekova@alatoo.edu.kg>
2024-11-07 16:05:10 +0600

    second bonus done

--- FirstFile.java
+++ FirstFile.java
@@ -7,12 +7,12 @@ public class FirstFile {
     static Scanner sc = new Scanner(System.in);
 
     static String[] encodedWord;
-    public static int points;
+    public static double points;
 
     static int toursNum = 3;
     static String[] usedWords;
     static int[] attemptsLeftInEachRound;
-    static int[] scoreInEachRound;
+    static double[] scoreInEachRound;
 
 
     public static void main(String[] args) {
@@ -26,7 +26,7 @@ public class FirstFile {
     public static void initializeGame(){
         usedWords = new String[toursNum];
         attemptsLeftInEachRound = new int[toursNum];
-        scoreInEachRound = new int[toursNum];
+        scoreInEachRound = new double[toursNum];
     }
 
     public static void tourLaunch(){
@@ -49,14 +49,27 @@ public class FirstFile {
 
             while(checkEncodedWord() && numberOfAttempts > 0){
                 println("Осталось попыток: " + numberOfAttempts );
-                String replacementLetter = chooseLetter(usedLetters);
 
-                usedLetters += replacementLetter;
-                if(replaceLetterInEncodedWord(word, replacementLetter)){
-                    println("Вы нашли букву!");
-                } else {
-                    println("Такой буквы нет...");
-                    numberOfAttempts--;
+                String action  = chooseAction();
+                if(action.equals("word")){
+                    String input = chooseWord(word);
+                    if (input.equalsIgnoreCase(word)) {
+                        points += word.length() + (word.length() * 0.05);
+                        println("Поздравляем! Вы угадали слово.");
+                    } else {
+                        println("Неверное слово. Игра окончена.");
+                        numberOfAttempts = 0;
+                    }
+                    break;
+                } else{
+                    String replacementLetter = chooseLetter(usedLetters);
+                    usedLetters += replacementLetter;
+                    if(replaceLetterInEncodedWord(word, replacementLetter)){
+                        println("Вы нашли букву!");
+                    } else {
+                        println("Такой буквы нет...");
+                        numberOfAttempts--;
+                    }
                 }
                 println("Состояние слова: " + Arrays.toString(encodedWord));
             }
@@ -67,7 +80,7 @@ public class FirstFile {
                 println("\nК сожалению, вы исчерпали все попытки...");
                 endGame(word);
             } else{
-                println("\nПоздравляем! Вы нашли все буквы!");
+                println("\nПоздравляем! Вы победили в этом раунде!");
                 endGame(word);
             }
             points = 0;
@@ -142,7 +155,7 @@ public class FirstFile {
     public static void endGame(String word){
         println("Игра окончена!");
         println("Отгадываемое слово: " + word);
-        printf("Количество набранных очков: %d.%n%n", points);
+        printf("Количество набранных очков: %.1f.%n%n", points);
     }
 
     public static boolean wasWordUsedBefore(String word){
@@ -154,10 +167,10 @@ public class FirstFile {
         return false;
     }
 
-    public static int sumOfScores(){
-        int sum = 0;
-        for(int i : scoreInEachRound){
-            sum += i;
+    public static double sumOfScores(){
+        double sum = 0;
+        for(double score : scoreInEachRound){
+            sum += score;
         }
         return sum;
     }
@@ -166,15 +179,15 @@ public class FirstFile {
         String[] rounds = {"- 1 -", "- 2 -", "- 3 -"};
         String lineHorizontal = "-";
         String lineVertical = "|";
-        println("\n" + lineHorizontal.repeat(14) + " Finish game " + lineHorizontal.repeat(14));
-        printf("%9s%5s%9s%5s%10s%n", "Round", lineVertical, "Score", lineVertical, "Attempts");
+        println("\n" + lineHorizontal.repeat(14) + " Итоги игры! " + lineHorizontal.repeat(14));
+        printf("%9s%5s%9s%5s%10s%n", "Раунд", lineVertical, "Очки", lineVertical, "Попытки");
         String decorativeLine = lineHorizontal.repeat(13) + lineVertical + lineHorizontal.repeat(13) + lineVertical + lineHorizontal.repeat(13);
         println(decorativeLine);
         for (int i = 0; i < toursNum; i++) {
-            printf("%9s%5s%7d%7s%7d%n", rounds[i], lineVertical, scoreInEachRound[i], lineVertical, attemptsLeftInEachRound[i]);
+            printf("%9s%5s%9.1f%5s%7d%n", rounds[i], lineVertical, scoreInEachRound[i], lineVertical, attemptsLeftInEachRound[i]);
         }
         println(decorativeLine);
-        printf("%9s%5s%7d%7s%7d%n", "Total", lineVertical, sumOfScores(), lineVertical, 1);
+        printf("%9s%5s%9.1f%5s%7d%n", "Всего", lineVertical, sumOfScores(), lineVertical, attemptsLeftInEachRound[2]);
     }
 
     public static boolean isGameRestarted(){
@@ -188,6 +201,37 @@ public class FirstFile {
                 return false;
             } else {
                 println("Неверный ввод. Пожалуйста, введите 'да' или 'нет'.");
+            }
+        }
+    }
+
+    public static String chooseAction(){
+        while (true) {
+            println("\nЧто вы хотите ввести?");
+            println("1. Ввести букву");
+            println("2. Ввести слово");
+            print("Введите номер (1 или 2): ");
+            String choice = sc.nextLine().strip();
+
+            if (choice.equals("1")) {
+                return "letter";
+            } else if (choice.equals("2")) {
+                return "word";
+            } else {
+                println("Неверный ввод! Пожалуйста, выберите 1 или 2.");
+            }
+        }
+    }
+
+    public static String chooseWord(String word) {
+        while (true) {
+            print("\nВведите ваше слово: ");
+            String input = sc.nextLine().strip().toLowerCase();
+
+            if (input.length() == word.length() && input.matches("[а-яё]+")) {
+                return input;
+            } else {
+                println("Неверное слово! Попробуйте еще раз.");
             }
         }
     }
